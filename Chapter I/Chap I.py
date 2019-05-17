e95ed7dd82b38ece51f03850e80df1cca492c234
mateuszsmrokowski <32837405+mateuszsmrokowski@users.noreply.github.com>
2019-05-17 08:58:04 +0200

    v 1.1

--- Chapter I/Chap I.py
+++ Chapter I/Chap I.py
@@ -1,6 +1,6 @@
 ListOfWords = []
 counter = 0
-with open("C:\Users\DieTenshi\Documents\GitHub\Skychallenge\Chapter I\Words.txt") as openfileobject:
+with open("C:\\Users\\DieTenshi\\Documents\\GitHub\\Skychallenge\\Chapter I\\Words.txt") as openfileobject:
     for line in openfileobject:
         ListOfWords.append(line.rstrip('\n').split(' '))
 
@@ -8,4 +8,4 @@ for i in ListOfWords:
     if (len(i) != len(set(i))) == True:
         counter +=1
 
-print "File have ", counter, " valid skyphrases"
+print ("File have ", counter, " valid skyphrases")
--- Chapter II/Chap II.py
+++ Chapter II/Chap II.py
@@ -1,9 +1,9 @@
 import re
 Result = 0
-file = open("C:\Users\DieTenshi\Documents\GitHub\Skychallenge\Chapter II\Cipher.txt", "r")
+file = open('C:\\Users\\DieTenshi\\Documents\\GitHub\\Skychallenge\\Chapter II\\Cipher.txt', 'r')
 numbers = [int(d) for d in re.findall(r'-?\d+', file.read())]
 
 for i in numbers:
     Result += i;
 
-print Result
+print (Result)
