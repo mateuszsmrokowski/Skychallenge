a09952fc17dbfa24a9df910f8c672eaebeb0b8c9
mateuszsmrokowski <32837405+mateuszsmrokowski@users.noreply.github.com>
2019-05-17 18:59:58 +0200

    Database models

--- Chapter_I/exams/models.py
+++ Chapter_I/exams/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+class Exams(models.Model):
+    scope_of_knowledge = models.CharField(max_length=200)
+    date = models.DateTimeField('Date of exam')
+
+
+class Grade(models.Model):
+    student = models.CharField(max_length=200)
+    points = models.IntegerField(default=0)
+    grade = models.IntegerField(default=0)
+    exam = models.ForeignKey(Exams, on_delete=models.CASCADE)
+
+class Students(models.Model):
+    Name = models.CharField(max_length=200)
+    Surname = models.CharField(max_length=200)
+    Age = models.IntegerField(default=0)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
